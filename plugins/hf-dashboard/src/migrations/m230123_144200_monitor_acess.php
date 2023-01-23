44144b00016abac0c47282fb9ed9296c6cc7c025
Ivan Pinheiro <ivaneletronica00@gmail.com>
2023-01-23 15:14:56 -0300

    Update project and migrations

---
+++ plugins/hf-dashboard/src/migrations/m230123_144200_monitor_acess.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace healthfirst\hfdashboard\migrations;
+
+use Craft;
+use craft\db\Migration;
+
+/**
+ * m230123_144200_monitor_acess migration.
+ */
+class m230123_144200_monitor_acess extends Migration
+{
+
+    /**
+     * @inheritdoc
+     */
+    public function safeUp(): bool
+    {
+        if (!$this->db->tableExists('{{%monitor_acess}}')) {
+            $this->createTable(
+                '{{%monitor_acess}}',
+                [
+                    'id' => $this->primaryKey(),
+
+                    'ip' => $this->string(25),
+                    'page' => $this->string(25),
+                    'formInfo' => $this->string(12),
+                    'dateCreated' => $this->dateTime(),
+                    'dateUpdated' => $this->dateTime(),
+                    'dateDeleted' => $this->dateTime()->null(),
+                    'uid' => $this->uid()
+                ]
+            );
+
+            $this->createIndex(
+                null,
+                '{{%monitor_acess}}',
+                'id',
+                false
+            );
+
+            $this->insert('{{%monitor_acess}}', [
+                'id' => 3250
+            ]);
+
+            $this->delete('{{%monitor_acess}}', [
+                'id' => 3250
+            ]);
+
+            Craft::$app->db->schema->refresh();
+        }
+
+        return true;
+    
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function safeDown(): bool
+    {
+        echo "m230123_144200_monitor_acess cannot be reverted.\n";
+        return false;
+    }
+}
