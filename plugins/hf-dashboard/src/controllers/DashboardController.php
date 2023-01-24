31be868bd8e740ecfe2b2b7548bca33a6ff7da1c
Arthur Nascimento <aairesvieiradonascim@healthfirst.org>
2023-01-24 10:46:05 -0300

    Created controller

---
+++ plugins/hf-dashboard/src/controllers/DashboardController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace healthfirst\hfdashboard\controllers;
+
+use Craft;
+use craft\web\Controller;
+use yii\web\Response;
+
+/**
+ * Dashboard controller
+ */
+class DashboardController extends Controller
+{
+    public $defaultAction = 'index';
+    protected array|int|bool $allowAnonymous = self::ALLOW_ANONYMOUS_NEVER;
+
+    /**
+     * hf-dashboard/dashboard action
+     */
+    public function actionIndex(): Response
+    {
+        $entries = craft.entries().section('media', 'insights').all();
+        dd($entries);
+
+
+        $accessData = Access::all();
+    }
+}
