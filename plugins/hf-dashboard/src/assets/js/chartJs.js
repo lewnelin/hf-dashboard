389bff7928971414243649055af20cdebc5f8f55
Arthur Aires <aaires@studiovisual.com.br>
2023-01-26 10:35:30 -0300

    Included database data to chart

--- plugins/hf-dashboard/src/assets/js/chartJs.js
+++ plugins/hf-dashboard/src/assets/js/chartJs.js
@@ -1,13 +1,23 @@
+$(document).ready(function () {
+    const graphdata = $('#graphdata').data("graph");
+    const graphtype = $('#graphtype').data("type");
+    const ctx = document.getElementById('myPieChart');
 
-  const ctx = document.getElementById('myPieChart');
-  
-  new Chart(ctx, {
-    type: 'pie',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5],
-      }]
-    }
-  });
+    var labels = graphdata.map(function(e) {
+        return e.page;
+    });
+    var data = graphdata.map(function(e) {
+        return e.cnt;
+    });
+
+    new Chart(ctx, {
+        type: 'pie', //graphtype
+        data: {
+            labels: labels,
+            datasets: [{
+                label: '# of Accesses',
+                data: data,
+            }]
+        }
+    });
+});
